6c320dcb10ded2a4ef18130c5954196fd20b61c6
Chris Hurt <chrishcoding@gmail.com>
2019-09-29 15:40:49 +1000

    Removed test route from server

--- server.js
+++ server.js
@@ -31,10 +31,6 @@ connection.once('open',()=>{
   console.log("MongoDB Database connection successful")
 })
 
-router.route('/').get((req,res)=>{
-  res.send(JSON.stringify({ Hello: "World"}));
- });
-
 const usersRouter = require('./routes/users')
 const dataCollectionsRouter = require('./routes/data')
 const guessSessionsRouter = require('./routes/guesses')
